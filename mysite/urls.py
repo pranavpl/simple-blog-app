2114ddac2a122abb84046c9432947e076328b489
Pranav.PL <48471754+pranavpl@users.noreply.github.com>
2024-08-09 10:20:11 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for mysite project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
 from django.contrib import admin
 from django.urls import path, include
@@ -43,7 +28,7 @@ urlpatterns = [
     path('must_authenticate/', must_authenticate_view, name="must_authenticate"),
     path('register/', registration_view, name="register"),
 
-    # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
+    # Password reset links 
     path('password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
          name='password_change_done'),
